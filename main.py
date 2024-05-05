d1528dfcebc9d297d181f017c6a600dc6ae00b28
Artem Oluwo <oluwoartem@gmail.com>
2024-05-06 01:06:43 +0300

    minor changes&patch func done

--- main.py
+++ main.py
@@ -68,43 +68,36 @@ class BudgetTracker:
             income_value = income[i] if i < len(income) else ''
             expense_value = expenses[i] if i < len(expenses) else ''
 
-            if income_value and expense_value:
-                text += (
-                    f"{income_value['date'].strftime("%d.%m.%Y"):<64}| "
-                    f"{expense_value['date'].strftime("%d.%m.%Y"):<64}\n"
-                    f"{income_value['category']:<64}| "
-                    f"{expense_value['category']:<64}\n"
-                    f"{income_value['amount']:<64}| "
-                    f"{expense_value['amount']:<64}\n"
-                    f"{income_value['description']:<64}| "
-                    f"{expense_value['description']:<64}\n\n"
-                )
-            elif income_value and not expense_value:
-                text += (
-                    f"{income_value['date'].strftime("%d.%m.%Y"):<64}| "
-                    f"{expense_value:<64}\n"
-                    f"{income_value['category']:<64}| "
-                    f"{expense_value:<64}\n"
-                    f"{income_value['amount']:<64}| "
-                    f"{expense_value:<64}\n"
-                    f"{income_value['description']:<64}| "
-                    f"{expense_value:<64}\n\n"
-                )
-            else:
-                text += (
-                    f"{income_value:<64}| "
-                    f"{expense_value['date'].strftime("%d.%m.%Y"):<64}\n"
-                    f"{income_value:<64}| "
-                    f"{expense_value['category']:<64}\n"
-                    f"{income_value:<64}| "
-                    f"{expense_value['amount']:<64}\n"
-                    f"{income_value:<64}| "
-                    f"{expense_value['description']:<64}\n\n"
-                )
+            text += (
+                f"{'ID: '+str(income_value['id'])
+                    if income_value else '':<64}| "
+                f"{'ID: '+str(expense_value['id'])
+                    if expense_value else '':<64}\n"
+
+                f"{'Дата: '+income_value['date'].strftime("%d.%m.%Y")
+                    if income_value else '':<64}| "
+                f"{'Дата: '+expense_value['date'].strftime("%d.%m.%Y")
+                    if expense_value else '':<64}\n"
+
+                f"{'Категория: '+income_value['category']
+                    if income_value else '':<64}| "
+                f"{'Категория: '+expense_value['category']
+                    if expense_value else '':<64}\n"
+
+                f"{'Сумма: '+str(income_value['amount'])
+                    if income_value else '':<64}| "
+                f"{'Сумма: '+str(expense_value['amount'])
+                    if expense_value else '':<64}\n"
+
+                f"{'Описание: '+income_value['description']
+                    if income_value else '':<64}| "
+                f"{'Описание: '+expense_value['description']
+                    if expense_value else '':<64}\n\n"
+            )
 
         return text
 
-    def validate_date(date: str) -> bool:
+    def _validate_date(date: str) -> bool:
         date_regex = r'^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[012])\.\d{4}$'
 
         if not re.match(date_regex, date):
@@ -118,7 +111,7 @@ class BudgetTracker:
 
         return True
 
-    def validate_amount(amount: str) -> bool:
+    def _validate_amount(amount: str) -> bool:
         amount_regex = r'^\d+(\.\d+)?$'
 
         if re.match(amount_regex, amount):
@@ -130,7 +123,7 @@ class BudgetTracker:
         """Adds a transaction"""
         while True:
             date: str = input("Enter a date with this format - <dd.mm.yyyy>: ")
-            if BudgetTracker.validate_date(date):
+            if BudgetTracker._validate_date(date):
                 break
             else:
                 print("Invalid date format or date does not exist."
@@ -150,11 +143,11 @@ class BudgetTracker:
             amount_str: str = input("Enter the amount of the transaction. "
                                     "(Only positive number, use '.' "
                                     "for a floating point): ")
-            if BudgetTracker.validate_amount(amount_str):
+            if BudgetTracker._validate_amount(amount_str):
+                amount: Decimal = round(Decimal(amount_str), 2)
                 break
             else:
                 print("Invalid amount format.")
-        amount: Decimal = round(Decimal(amount_str), 2)
 
         while True:
             description: str = input('Enter the description '
@@ -206,6 +199,145 @@ class BudgetTracker:
                 writer.writerow(data_list)
             return BudgetTracker.balance()
 
+    def patch_transaction() -> str | None:
+        """Change a transaction"""
+        all_transactions: List[Dict] = BudgetTracker.get_all_data()
+        if not all_transactions:
+            print('No transactions to patch.')
+            return
+
+        # Получаем id
+        while True:
+            try:
+                id_: int = int(input('Enter ID of the transaction: '))
+            except ValueError:
+                print('Invalid value. Please enter a valid number.')
+                continue
+
+            id_found = False
+            for trans in all_transactions:
+                if trans['id'] == id_:
+                    id_found = True
+                    selected_trans: Dict = trans
+                    break
+            if id_found:
+                break
+            else:
+                print('ID not found. Try again')
+
+        # Получаем дату и форматируем для будущей проверки
+        while True:
+            date: str = input("Enter a date with this format - <dd.mm.yyyy>."
+                              "\nLeave empty to skip: ")
+            if date == '':
+                date: str = selected_trans['date'].strftime("%d.%m.%Y")
+                break
+            elif BudgetTracker._validate_date(date):
+                break
+            else:
+                print("Invalid date format or date does not exist."
+                      "Please enter in dd.mm.yyyy format.")
+        date: datetime.date = datetime.date(int(date.split('.')[2]),
+                                            int(date.split('.')[1]),
+                                            int(date.split('.')[0]),)
+
+        # Получаем категорию
+        while True:
+            category: str = input("Enter a category - доход/расход."
+                                  "\nLeave empty to skip: ").lower()
+            match category:
+                case 'доход':
+                    break
+                case 'расход':
+                    break
+                case '':
+                    category: str = selected_trans['category']
+                    break
+                case _:
+                    print('Incorrect format.')
+
+        # Получаем сумму
+        while True:
+            amount_str: str = input("Enter the amount of the transaction. \n"
+                                    "(Only positive number, use '.' "
+                                    "for a floating point).\n"
+                                    "Leave empty to skip: ")
+            if amount_str == '':
+                amount: Decimal = selected_trans['amount']
+                break
+            elif BudgetTracker._validate_amount(amount_str):
+                amount: Decimal = round(Decimal(amount_str), 2)
+                break
+            else:
+                print("Invalid amount format.")
+
+        # Получаем описание
+        while True:
+            description: str = input('Enter the description '
+                                     'under 64 symbols.\n'
+                                     'Leave empty to skip: ')
+            if description == '':
+                description: str = selected_trans['description']
+                break
+            elif len(description) > 64:
+                print('Description too long.')
+            else:
+                break
+
+        # Рассчитываем баланс исходя из предыдущей записи
+        for index, trans in enumerate(all_transactions):
+            if trans == selected_trans and index != 1:
+                balance: Decimal = (
+                    all_transactions[index-1]['balance'] + amount
+                    if category == 'доход'
+                    else all_transactions[index-1]['balance'] - amount)
+            elif trans == selected_trans and index == 1:
+                balance: Decimal = (
+                    amount if category == 'доход'
+                    else - amount)
+
+        patched_transaction: Dict = {
+            'id': id_, 'date': date, 'category': category,
+            'amount': amount, 'description': description,
+            'balance': round(balance, 2)
+        }
+
+        # Проверяем наличие изменений
+        if patched_transaction == selected_trans:
+            return BudgetTracker.balance()
+
+        # Расчитываем баланс для всех последующих записей
+        transaction_found = False
+        for index, trans in enumerate(all_transactions):
+            if trans == selected_trans:
+                all_transactions[index] = patched_transaction
+                transaction_found = True
+
+            if transaction_found:
+                balance: Decimal = (
+                    all_transactions[index-1]['balance'] + amount
+                    if category == 'доход'
+                    else all_transactions[index-1]['balance'] - amount)
+                all_transactions[index]['balance'] = balance
+
+        # Записываем данные заново в файл
+        with open('database.csv', 'w', encoding='UTF-8') as file:
+            writer = csv.writer(file)
+            head = [
+                'id', 'date', 'category', 'amount', 'description', 'balance'
+                ]
+            writer.writerow(head)
+            for index, trans in enumerate(all_transactions):
+                data = [
+                    all_transactions[index]['id'],
+                    all_transactions[index]['date'].strftime("%d.%m.%Y"),
+                    all_transactions[index]['category'],
+                    all_transactions[index]['amount'],
+                    all_transactions[index]['description'],
+                    all_transactions[index]['balance'],
+                ]
+                writer.writerow(data)
+        return BudgetTracker.balance()
 
     def main() -> None:
         print('Client started! Type "help" for all commands')
@@ -214,4 +346,4 @@ class BudgetTracker:
 
 
 if __name__ == "__main__":
-    print(BudgetTracker.balance())
+    print(BudgetTracker.patch_transaction())
